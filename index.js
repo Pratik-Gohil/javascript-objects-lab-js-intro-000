d37f86e1a04d34206f832281bf1d31a8e12dd587
Pratik <pr.tikgohil@gmail.com>
2020-02-22 17:42:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function updateObjectWithKeyAndValue(object, key, value) {
-  var obj = Object.assign({}, object) 
+  var obj = Object.assign({}, object)
   return Object.assign(obj, obj[key] = value)
 }
